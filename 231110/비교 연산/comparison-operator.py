15a4ce235d5b4a9e242544275c3227c9528f1b11
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-11-10 23:54:47 +0900

    Title: 비교 연산, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231110/비교 연산/comparison-operator.py
@@ -0,0 +1,35 @@
+k = input()
+l = k.split()
+
+a = int(l[0])
+b = int(l[1])
+
+if a >= b:
+    print(1)
+else:
+    print(0)
+
+if a > b:
+    print(1)
+else:
+    print(0)
+
+if b >= a:
+    print(1)
+else:
+    print(0)
+
+if b > a:
+    print(1)
+else:
+    print(0)
+
+if a == b:
+    print(1)
+else:
+    print(0)
+    
+if a != b:
+    print(1)
+else:
+    print(0)
