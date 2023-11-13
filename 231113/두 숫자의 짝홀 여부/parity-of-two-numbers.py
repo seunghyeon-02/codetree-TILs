0def56b275af35c94f33a19fef214329e593f9de
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-11-13 13:29:09 +0900

    Title: 두 숫자의 짝홀 여부, Time: 128ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 231113/두 숫자의 짝홀 여부/parity-of-two-numbers.py
@@ -0,0 +1,15 @@
+k = input()
+l = k.split()
+
+a = int(l[0])
+b = int(l[1])
+
+if  a % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+if  b % 2 == 0:
+    print("even")
+else:
+    print("odd")
