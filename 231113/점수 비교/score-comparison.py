b8f980f9bae87097dfac8d3546312de25cc189ef
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-11-13 15:42:26 +0900

    Title: 점수 비교, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231113/점수 비교/score-comparison.py
@@ -0,0 +1,16 @@
+k = input()
+m = k.split()
+
+math_Score_A = int(m[0])
+eng_Score_A = int(m[1])
+
+l = input()
+n = l.split()
+
+math_Score_B = int(l[0])
+eng_Score_B = int(l[1])
+
+if math_Score_A > math_Score_B and eng_Score_A > eng_Score_B:
+    print(1)
+else:
+    print(0)
