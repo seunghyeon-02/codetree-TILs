cf0669ccb288217a2634e52c2be705cff8f149aa
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-11-13 15:42:52 +0900

    Title: 점수 비교, Time: 119ms, Memory: 25MB, Status: Passed - CodeTree

--- 231113/점수 비교/score-comparison.py
+++ 231113/점수 비교/score-comparison.py
@@ -6,9 +6,8 @@ eng_Score_A = int(m[1])
 
 l = input()
 n = l.split()
-
-math_Score_B = int(l[0])
-eng_Score_B = int(l[1])
+math_Score_B = int(n[0])
+eng_Score_B = int(n[1])
 
 if math_Score_A > math_Score_B and eng_Score_A > eng_Score_B:
     print(1)
