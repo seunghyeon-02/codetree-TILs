6d072c8934e16fe6c75c9e58ebee16239e400861
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-11-14 13:19:14 +0900

    Title: 장학금, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 231114/장학금/scholarship.py
+++ 231114/장학금/scholarship.py
@@ -7,9 +7,9 @@ last_score = int(l[1])
 
 if mid_score >= 90:
     if last_score >=95:
-        print(10)
+        print(100000)
     elif last_score >=90:
-        print(5)
+        print(50000)
     else:
         print(0)
 else:
