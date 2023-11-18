54d7c617d1fd68a9a3c4d96719e266d851affc2a
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-11-19 02:21:03 +0900

    Title: a/b 출력, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231119/a/b 출력/a-divide-b.py
@@ -0,0 +1,14 @@
+k = input()
+l = k.split()
+
+a = int(l[0])
+b = int(l[1])
+
+d = a // b
+print(f"{d}.", end ="")
+
+for i in range(20):
+    c = 10 * (a % b)
+    d = c / b
+    print(f"{d:.0f}", end = "")
+    a = c % b
