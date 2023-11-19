8b3ad93da57eda7c54f00f69600a1136bb24e270
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-11-19 22:52:17 +0900

    Title: 369 게임, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 231119/369 게임/369-game.py
+++ 231119/369 게임/369-game.py
@@ -1,12 +1,22 @@
 n = int(input())
 i = 1
-
+k = "3"
+l = "6"
+m = "9"
 while i <= n:
     if i % 3 != 0:
-        if 30 <= i and i <= 39:
-            print(0, end =" ")
-            i += 1
+        i = str(i)
+        if k in i:
+            print(0, end = " ")
+            i = int(i) + 1
+        elif l in i:
+            print(0, end = " ")
+            i = int(i) + 1
+        elif m in i:
+            print(0, end = " ")
+            i = int(i) + 1   
         else:
+            i = int(i)
             print(i, end =" ")
             i += 1
     else:
