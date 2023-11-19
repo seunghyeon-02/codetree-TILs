c0872dd9be748a5681e519ce0421361f9eb08ffb
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-11-19 22:18:51 +0900

    Title: 369 게임, Time: 5092ms, Memory: 242MB, Status: Time Limit Exceed - CodeTree

--- 231119/369 게임/369-game.py
+++ 231119/369 게임/369-game.py
@@ -2,8 +2,11 @@ n = int(input())
 i = 1
 while i <= n:
     if i % 3 != 0:
-        print(i, end =" ")
-        i += 1
+        if 30<= i <= 39:
+            print(0)
+        else:
+            print(i, end =" ")
+            i += 1
     else:
         print(0, end = " ")
         i += 1
