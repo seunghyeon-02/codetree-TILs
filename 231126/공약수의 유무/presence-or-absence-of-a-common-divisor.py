0b2d8dc69496349d1c78c291e8f344e6b4ca0d2f
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-11-26 22:50:38 +0900

    Title: 공약수의 유무, Time: 252ms, Memory: 25MB, Status: Passed - CodeTree

--- 231126/공약수의 유무/presence-or-absence-of-a-common-divisor.py
+++ 231126/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -8,7 +8,7 @@ f = 1
 cnt = 0
 
 while t <= 1920:
-    if 1920 % t == 0:
+    if 1920 % t == 0 and 2880 % t == 0:
         for i in range(a, b+1):
             if i == t:
                 cnt += 1
@@ -16,14 +16,7 @@ while t <= 1920:
     else:
         t += 1
 
-while f <= 2880:
-    if 2880 % f == 0:
-        for i in range(a, b+1):
-            if i == f:
-                cnt += 1
-        f += 1
-    else:
-        f += 1
+
 
 if cnt >= 1:
     print(1)
