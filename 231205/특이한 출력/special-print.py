167141c7f11390e4c3f8a04e6bcc6e9dcb7b7b07
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-12-05 23:53:01 +0900

    Title: 특이한 출력, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 231205/특이한 출력/special-print.py
+++ 231205/특이한 출력/special-print.py
@@ -2,7 +2,7 @@ n = int(input())
 
 for i in range(1,n+1):
     for k in range(1,n+1):
-        if i + k == 4:
+        if (i + k) % 4 == 0:
             print(f"({i}, {k})")
         else:
             print(f"({i}, {k})", end =" ")
