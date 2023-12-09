7e6514000a6e621c2f90c1ecc0ff567ab5046d15
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-12-09 20:04:33 +0900

    Title: 십의 자리 숫자, Time: 134ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 231209/십의 자리 숫자/number-of-tens-digit.py
@@ -0,0 +1,18 @@
+arr = list(map(int, input().split()))
+count_arr = [0] * 9
+
+n = len(arr)
+for i in range(n):
+    if arr[i] == 0:
+        arr = arr[:i]
+        break
+
+n1 = len(arr)
+
+for k in range(n1):
+    if arr[k] >= 10:
+        cnt = arr[k] // 10
+        count_arr[cnt-1] += 1
+
+for j in range(1,10):
+    print(f"{j} - {count_arr[j-1]}")
