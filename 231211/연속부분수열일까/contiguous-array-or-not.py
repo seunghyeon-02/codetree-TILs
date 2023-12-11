1680013807391efbd39bdb0f8ce571e4e0aa7743
seunghyeon-02 <150220082+seunghyeon-02@users.noreply.github.com>
2023-12-11 23:17:28 +0900

    Title: 연속부분수열일까, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 231211/연속부분수열일까/contiguous-array-or-not.py
+++ 231211/연속부분수열일까/contiguous-array-or-not.py
@@ -12,7 +12,7 @@ else:
     #arr_A 에 있는 arr_B의 첫번째 원소의 갯수만큼 반복한다.
     for h in range(count_locate):
         #만약 리스트 슬라이싱으로 n1의 길이가 작아진다면 멈추고 No 출력.
-        if len(arr_A) < len_B or arr_B[0] not in arr_A:
+        if (len(arr_A) < len_B) or (arr_B[0] not in arr_A):
             print("No")
             break    
         # 이 help select는 얼마나 일치하는지 알려주는 역할을 함.
